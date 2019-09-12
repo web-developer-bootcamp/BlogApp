085a52ba688bc4723ebd63743b91527c12863548
Yintong <zhouyintong96@gmail.com>
2019-09-12 14:59:17 +0200

    add index with foreach

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 
 //App config
-mongoose.connect("mongodb://localhost:27017/restful_blogApp", { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/restful_blogApp', { useNewUrlParser: true, useUnifiedTopology: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,10 +19,25 @@ var blogSchema = new mongoose.Schema({
         default: Date.now
     }
 });
-var Blog = mongoose.model("Blog", blogSchema); 
+var Blog = mongoose.model("Blog", blogSchema);
+
+//Blog.create({
+//title: "Test Blog",
+//image: "https://cdn.pixabay.com/photo/2015/03/26/09/54/pug-690566_960_720.jpg",
+//body: "Hello dog post!"
+//});
 
 //Restful routes
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    });
+});
 
-
+app.get("/", (req, res) => res.redirect("blogs"));
 
 app.listen(3000, () => console.log("Blog Server is started!"))
